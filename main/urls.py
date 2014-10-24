7aa71df683b1e406d15f174f06da96083becad70
Paulina Lach <smig007@wp.pl>
2014-10-25 01:53:11 +0200

    CRUD for Album, Category

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,27 @@
 from django.conf.urls import patterns, url
-from .views import PhotographIndex, PhotographCreate, PhotographShow, PhotographUpdate, PhotographDelete
+from .views import CategoryIndex, CategoryCreate, CategoryShow, CategoryUpdate, CategoryDelete, AlbumIndex, AlbumCreate, AlbumShow, AlbumUpdate, AlbumDelete, PhotographIndex, PhotographCreate, PhotographShow, PhotographUpdate, PhotographDelete
 
 urlpatterns = patterns('',
+    url(r'^categories/$', CategoryIndex.as_view(), name='category_index'),
+    url(r'^categories/create/$', CategoryCreate.as_view(), name='category_create'),
+    url(r'^categories/(?P<pk>\d+)/$', CategoryShow.as_view(), name='category_show'),
+    url(r'^categories/(?P<pk>\d+)/update$', CategoryUpdate.as_view(), name='category_update'),
+    url(r'^categories/(?P<pk>\d+)/delete', CategoryDelete.as_view(), name='category_delete'),
+
+
+    url(r'^albums/$', AlbumIndex.as_view(), name='album_index'),
+    url(r'^albums/create/$', AlbumCreate.as_view(), name='album_create'),
+    url(r'^albums/(?P<pk>\d+)/$', AlbumShow.as_view(), name='album_show'),
+    url(r'^albums/(?P<pk>\d+)/update$', AlbumUpdate.as_view(), name='album_update'),
+    url(r'^albums/(?P<pk>\d+)/delete', AlbumDelete.as_view(), name='album_delete'),
+
+
     url(r'^photographs/$', PhotographIndex.as_view(), name='photograph_index'),
     url(r'^photographs/create/$', PhotographCreate.as_view(), name='photograph_create'),
     url(r'^photographs/(?P<pk>\d+)/$', PhotographShow.as_view(), name='photograph_show'),
     url(r'^photographs/(?P<pk>\d+)/update$', PhotographUpdate.as_view(), name='photograph_update'),
     url(r'^photographs/(?P<pk>\d+)/delete', PhotographDelete.as_view(), name='photograph_delete'),
+
+
 )
+
--- main/views.py
+++ main/views.py
@@ -2,12 +2,58 @@ __author__ = 'paulina'
 
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
-from .models import Photograph
+from .models import Photograph, Album, Category
 
+category_field = ['name']
+album_field = ['name', 'user']
 photograph_field = ['name', 'data', 'comment', 'latitude', 'longitude', 'user', 'category', 'albums']
 
 def home(request):
     return render(request, 'home.html')
+
+class CategoryIndex(ListView):
+    model = Category
+
+class CategoryCreate(CreateView):
+    model = Category
+    success_url = '/'
+    fields = category_field
+
+class CategoryShow(DetailView):
+    model = Category
+
+class CategoryUpdate(UpdateView):
+    model = Category
+    success_url = '/'
+    fields = category_field
+
+class CategoryDelete(DeleteView):
+    model = Category
+    success_url = '/'
+
+
+
+class AlbumIndex(ListView):
+    model = Album
+
+class AlbumCreate(CreateView):
+    model = Album
+    success_url = '/'
+    fields = album_field
+
+class AlbumShow(DetailView):
+    model = Album
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    success_url = '/'
+    fields = album_field
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = '/'
+
+
 
 class PhotographIndex(ListView):
     model = Photograph
