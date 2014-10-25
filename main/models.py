361c072c97a72083be2e70df49b47d0ad64699dc
Paulina Lach <smig007@wp.pl>
2014-10-25 20:37:34 +0200

    Initial show.

--- main/models.py
+++ main/models.py
@@ -20,9 +20,13 @@ class Photograph(models.Model):
     comment = models.TextField()
     latitude = models.FloatField()
     longitude = models.FloatField()
+    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     category = models.ForeignKey(Category)
     albums = models.ManyToManyField(Album)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        get_latest_by = 'creation_date'
